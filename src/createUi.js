39aa6348d36b6978015d1931ad6f6de618af1d90
Toma Bogdan <tbogdane@TBE>
2023-10-31 18:08:48 +0200

    created header with button for new notes when project selected

--- src/createUi.js
+++ src/createUi.js
@@ -22,17 +22,32 @@ const createNewProjectCardUi = () => {
     card.classList.add("hidden");
     // Unregister the event listener
     submitButton.removeEventListener("click", newProjectSubmitButtonListener);
-  }
+  };
   // Add the event listener
   submitButton.addEventListener("click", newProjectSubmitButtonListener);
-}
+};
+
+const projectsMain = () => {
+  const main = document.getElementById("main");
+  const topMenu = document.createElement("div");
+  topMenu.id = "main-create-new-note-header";
+  //topMenu.classList.add("fade-in");
+  const newNoteBtn = document.createElement("button");
+  newNoteBtn.id = "main-create-new-note-btn";
+  newNoteBtn.textContent = "+ Create new note";
+  main.appendChild(topMenu);
+  topMenu.appendChild(newNoteBtn);
+};
 
 //creates new buttons for the new user-made projects
 const createProjectMenuBtns = (name) => {
   const newProjectBtn = document.createElement("button");
   newProjectBtn.classList.add("menu-btn");
   newProjectBtn.textContent = name;
+  newProjectBtn.addEventListener("click", () => {
+    projectsMain(name);
+  });
   return newProjectBtn;
-}
+};
 
 export { createNewProjectCardUi, createProjectMenuBtns };
